0446857ac18d3677a807cb4fbfa0f973db6bc820
Melanie Moore <mcmoore@uindy.edu>
2018-05-16 23:46:24 -0400

    Homework function added

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ console.log("working")
 const form = document.querySelector('form#studentForm')
 
 function updateClass(){
-var update= document.getElementById("studentForm")
-var newClass = document.createElement(li)
-li.text = "Pie"
-update.add(li)
+    debugger
+var change = document.getElementById("className")
+var li = document.createElement('li')
+//li.text = "Pie"
+var x = document.createTextNode(change)
+ li.appendChild(x)
+
+ if (change === '') {
+    alert("You must write something!")
+  } else {
+    document.getElementById("myList").appendChild(li)
+  }
+  document.getElementById("className") == ""
+
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
 }
+
+
