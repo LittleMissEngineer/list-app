cf24b3a8be92f742d74d36c46134488a5d8cb16c
Melanie Moore <mcmoore@uindy.edu>
2018-05-16 20:07:04 -0400

    Javascript added to code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("working")
+
+const form = document.querySelector('form#studentForm')
+
+function updateClass(){
+var update= document.getElementById("studentForm")
+var newClass = document.createElement(li)
+li.text = "Pie"
+update.add(li)
+}
