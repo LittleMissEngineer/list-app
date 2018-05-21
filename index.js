df0c1f405e03839c15291e82fe7c30d3932787e6
Melanie Moore <mcmoore@uindy.edu>
2018-05-21 09:52:53 -0400

    Used ES6 class

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = {
-  init(selectors) {
+class App {
+  constructor(selectors) {
     this.flicks = []
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
@@ -11,7 +11,7 @@ const app = {
         ev.preventDefault()
         this.handleSubmit(ev)
       })
-  },
+  }
 
   removeFlick(flick ,ev){
 const item = ev.target.closest('.flick')
@@ -21,7 +21,7 @@ const i = this.flicks.indexOf(flick)
 this.flicks.splice(i , 1)
 // const id = item.dataset.id
 
-  },
+  }
   renderListItem(flick) {
     const item = this.template.cloneNode(true)
     item.classList.remove('template')
@@ -34,7 +34,7 @@ this.flicks.splice(i , 1)
       .addEventListener('click',this.removeFlick.bind(this,flick ))
 
     return item
-  },
+  }
 
   handleSubmit(ev) {
     const f = ev.target
@@ -49,7 +49,7 @@ this.flicks.splice(i , 1)
     this.list.insertBefore(item, this.list.firstElementChild)
 
     f.reset()
-  },
+  }
 }
 
 // function deleteLi(){
@@ -72,10 +72,9 @@ document.getElementById('classSchedules').style.background = color
 }
 
   
-
-
-app.init({
+const app = new App({
   formSelector: '#flickForm',
   listSelector: '#flickList',
   templateSelector: '.flick.template',
 })
+
